9093e07385ecc011d232aee12b2304317ea4445d
swatitonge <swatitonge@yahoo.co.in>
2017-11-19 00:36:44 -0500

    update Nov 19

--- Website/Models/IdentityModels.cs
+++ Website/Models/IdentityModels.cs
@@ -10,11 +10,9 @@ namespace Website.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        /*[Required]
-        public string FirstName { get; set; }
-
-        [Required]
-        public string LastName { get; set; }*/
+        
+        public int Category { get; set; }
+ 
 
         
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
