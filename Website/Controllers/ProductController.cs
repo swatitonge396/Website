e3b5a6e5deb70fe676d50edf9bf771793ec337af
swatitonge <swatitonge@yahoo.co.in>
2017-11-20 13:28:20 -0500

    update to product controller

--- Website/Controllers/ProductController.cs
+++ Website/Controllers/ProductController.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Website.Models;
 
 namespace Website.Controllers
 {
-    public class ProductController : Controller
+    // [Authorize]
+    public class ProductsController : Controller
     {
-        // GET: Product
+        private MyDbContext db = new MyDbContext();
+
+        // GET: Products
         public ActionResult Index()
         {
-            return View();
+            var list = (from product in db.Products
+                        where product.Category == 1000
+                        select product).ToList();
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View(list);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var list = (from product in db.Products
+                        where product.Category == id
+                        select product).ToList();
+            if (list == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Index", list);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
